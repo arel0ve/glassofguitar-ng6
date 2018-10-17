73e2b488f10148effd867f25750406d7c0f83b20
arel0ve <vpsyol@gmail.com>
2018-10-17 17:26:14 +0300

    modified some specs

--- front-ng/src/app/api/get-user/get-user.service.spec.ts
+++ front-ng/src/app/api/get-user/get-user.service.spec.ts
@@ -1,15 +1,38 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { GetUserService } from './get-user.service';
+import {HttpClient, HttpClientModule, HttpEvent, HttpHandler, HttpRequest} from '@angular/common/http';
+import {fail} from 'assert';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 describe('GetUserService', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let getUserService: GetUserService;
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [GetUserService]
+      imports: [HttpClientTestingModule],
+      providers: [GetUserService, HttpClient, HttpHandler]
     });
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+    getUserService = TestBed.get(GetUserService);
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
   });
 
   it('should be created', inject([GetUserService], (service: GetUserService) => {
     expect(service).toBeTruthy();
   }));
+
+
+  it('user\'s tag should be \'dev\'',
+    () => {
+      getUserService.getUser({login: 'arel0ve', songId: '0'}).subscribe(user => {
+        expect(user['tag']).toBe('dev');
+      }, fail);
+  });
 });
--- front-ng/src/app/app.component.spec.ts
+++ front-ng/src/app/app.component.spec.ts
@@ -13,10 +13,10 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'angular-prj'`, async(() => {
+  it(`should have as title 'Glass of Guitar'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-prj');
+    expect(app.title).toEqual('Glass of Guitar');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
