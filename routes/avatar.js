ca30a309f74d44324a595a08f6230787f6708711
arel0ve <vpsyol@gmail.com>
2019-01-29 16:37:21 +0200

    Make post-avatar on back-end

--- routes/avatar.js
+++ routes/avatar.js
@@ -10,6 +10,8 @@ const multerS3 = require('multer-s3');
 
 const AWS = require('aws-sdk');
 
+let fineName = '';
+
 AWS.config.update({
   secretAccessKey: 'Mr1JDAx/ql+wObd1ifhqLn89ZtEqenjLJZC8bPxc',
   accessKeyId: 'AKIAJO2XWUY4J5CVIPPQ',
@@ -25,46 +27,66 @@ const upload = multer({
     acl: 'public-read-write',
     key: async function (req, file, cb) {
 
-      // if (!req.body.token) {
-      //   return 403;
-      // }
-      //
-      // if (!req.body.login) {
-      //   return 402;
-      // }
-      //
-      // const decodedToken = await admin.auth().verifyIdToken(req.body.token);
-      // if (!decodedToken || !decodedToken.uid) {
-      //   return 403;
-      // }
-      // const uid = decodedToken.uid;
-      //
-      // let user = await User.findOne({ uid });
-      //
-      // if (!user) {
-      //   return 404;
-      // }
+      fineName = Date.now().toString();
 
-      let name = Date.now().toString();
-
-      // user.photoUrl = `https://s3.amazonaws.com/glassof-guitar/${name}`;
-      // await user.save();
-
-      cb(null, name);
+      cb(null, fineName);
     }
   })
 });
 
 const User = require('../models/user').User;
 
-router.post('/:login', upload.array('fileUploaded', 1), postAvatar);
+router.post('/', upload.array('fileUploaded', 1), postAvatar);
 router.get('/:login', getAvatar);
 
-function postAvatar(req, res, next) {
-  res.status(200).json({
-    status: 'ok',
-    message: 'Saving successful'
-  });
+async function postAvatar(req, res, next) {
+  try {
+    if (!req.headers.token) {
+      res.status(403).json({
+        status: 'error',
+        message: 'Not authenticated'
+      });
+      return;
+    }
+
+    const decodedToken = await admin.auth().verifyIdToken(req.headers.token);
+    if (!decodedToken || !decodedToken.uid) {
+      res.status(403).json({
+        status: 'error',
+        message: 'Wrong token'
+      });
+      return;
+    }
+
+    let user = await User.findOne({ uid: decodedToken.uid });
+
+    if (!user) {
+      res.status(404).json({
+        status: 'error',
+        message: 'User not found'
+      });
+      return;
+    }
+
+    user.photoUrl = `https://s3.amazonaws.com/glassof-guitar/${fineName}`;
+
+    user = await user.save();
+
+    res.status(200).json({
+      login: user.login,
+      photoUrl: user.photoUrl,
+      status: 'ok',
+      message: 'Saving successful'
+    });
+
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      status: "error",
+      message: "Server error"
+    });
+  }
+
 }
 
 async function getAvatar(req, res, next) {
